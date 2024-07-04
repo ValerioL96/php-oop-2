e6c69e2a70621949d3a2c813a5b745b6e3a0ce25
ValerioL96 <v.lembo1396@gmail.com>
2024-07-04 17:29:30 +0200

    Added subclasses of the products class

--- classes/accessories.php
+++ classes/accessories.php
@@ -4,4 +4,21 @@ require_once __DIR__ . '/products.php';
 
 class Accessories extends Products{
 
+    private $size;
+    private $function;
+
+    public function __construct($name, $brand, $price, $image, $is_available, Category $category, $size, $function) {
+        parent:: __construct($name, $brand, $price, $image, $is_available, $category);
+        $this->size = $size;
+        $this->function = $function;
+    }
+
+    public function getSize(){
+        return $this->size;
+    }
+
+    public function getFunction(){
+        return $this->function;
+    }
+
 }
--- classes/games.php
+++ classes/games.php
@@ -4,4 +4,21 @@ require_once __DIR__ . '/products.php';
 
 class Games extends Products{
 
+    private $material;
+    private $color;
+
+    public function __construct($name, $brand, $price, $image, $is_available, Category $category, $material, $color) {
+        parent:: __construct($name, $brand, $price, $image, $is_available, $category);
+        $this->material = $material;
+        $this->color = $color;
+    }
+
+    public function getMaterial(){
+        return $this->material;
+    }
+
+    public function getColor(){
+        return $this->color;
+    }
+
 }
