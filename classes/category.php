04eb788c13d6d97b4033f4c459b5e7253eed53a8
ValerioL96 <v.lembo1396@gmail.com>
2024-07-05 13:24:54 +0200

    Fixed db.php and category class; Added condition in html.php file

--- classes/category.php
+++ classes/category.php
@@ -2,18 +2,14 @@
 
 class Category{
     private $name;
-    private $icon;
 
-    public function __construct($name, $icon) {
+    public function __construct($name) {
         $this->name = $name;
-        $this->icon = $icon;
     }
 
     public function getName(){
         return $this->name;
     }
 
-    public function getIcon(){
-        return $this->icon;
-    }
+
 }
--- index.php
+++ index.php
@@ -39,20 +39,53 @@ require_once __DIR__ . '/db/db.php';
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
     crossorigin="anonymous">
 
+<!--fontawesome-->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" 
+    integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" 
+    crossorigin="anonymous" 
+    referrerpolicy="no-referrer" />
+
 
 </head>
 <body>
+    <header class="conainer py-4 px-5 mx-5 my-3">
+        <div class="row">
+            <div class="col-12">
+                <h1 >Boolshop</h1>
+            </div>
+        </div>
+    </header>
 
     <main class="container">
+    <div class="row">
+            <div class="col-12 text-body-tertiary">
+                <h2>I nostri prodotti</h2>
+            </div>
+        </div>
         <div class="row">
             <?php foreach ( $products as $product ){?>
                 <article class="col-4">
-                    <div class="card">
+                    <div class="card mb-3">
                         <div class="card-body">
+                            <div class="card-img">
+                                <img src="<?php echo $product->getImage()?>" alt="" class="img-fluid">
+                            </div>
                             <h5 class="card-title">
                                 <?php echo $product->getName()?>
                             </h5>
-                            <p class="card-text"><?php echo $product->getCategory()?></p>
+                            <p class="card-text"><?php echo $product->getCategory()->getName() ?></p>
+                            <p class="card-text">Prezzo: &euro; <?php echo $product->getPrice() ?></p>
+                            <?php if(get_class($product)=== "Food"){?>
+                                <p class="card-text">Peso netto:  <?php echo $product->getWeight() ?> g</p>
+                                <p class="card-text">Ingredienti:  <?php echo $product->getIngredients()?></p>
+                            <?php } elseif(get_class($product)=== "Accessories"){?>
+                                <p class="card-text">Materiale:  <?php echo $product->getMaterial() ?></p>
+                                <p class="card-text">Dimensioni:  <?php echo $product->getSize()?></p>
+                            <?php } elseif(get_class($product)=== "Games"){?>
+                                <p class="card-text">Caratteristiche:  <?php echo $product->getCharacteristics() ?></p>
+                                <p class="card-text">Dimensioni:  <?php echo $product->getSize()?></p>
+                            <?php } ?>
                         </div>
                     </div>
                 </article>
